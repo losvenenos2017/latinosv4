54b5bd4d4c140687f73c1b06fcf5b409e2fbca73
losvenenos2017 <robinperez2490@gmail.com>
2016-12-06 21:24:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="15">
+        <title>LatinosBarberShop</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <script src="js/slider.js" type="text/javascript"></script>
+    </head>
+    
+    <body>
+        <div id="imagen-derecha">
+                <img src="img/derecho2.png">
+        </div>
+        
+        <header>
+            <nav> 
+                <ul>
+                    <li> <a href=""> Inicio</a></li>
+                    <li> <a href=""> Nosotros</a></li>
+                    <li> <a href=""> Galeria</a></li>
+                    <li> <a href=""> Contactenos</a></li>
+                    <li> <a href=""> Login</a></li>
+                    <li> <a href="" id="Registro"> Registro</a></li>
+                </ul>
+            </nav>
+        </header>    
+        <!--    -->
+        <div id='slider'>
+            <div class="slider-inner">
+                <ul>
+                    <li> <a class="ns-img" href="img/slide1.jpg"></a> </li>
+                    <li> <a class="ns-img" href="img/slide2.jpg"></a> </li>
+                    <li> <a class="ns-img" href="img/slide4.jpg"></a> </li>
+                    <li> <a class="ns-img" href="img/slide5.jpg"></a> </li>
+                </ul>    
+            </div>
+        </div>
+        
+        <div id="columna">
+            <div id="columna1"> 
+                <h1><center>OFICIO</center></h1>
+                    <p>Queremos ayudar a relanzar uno de los oficios más valorados para el público masculino y femenino.
+                        Te encontrarás en un entorno confortable y con toda la esencia de las barberías más tradicionales
+                        americanas.
+                    </p>
+            </div>
+
+            <div id="columna2"> 
+                <h1><center>SOBRE NOSOTROS</center></h1>
+                    <p>Barberos y estilistas especializados que cuidan de tu look. 
+                        Sabemos lo que te quedará mejor, como cortarte el pelo y arreglarte la barba.
+                        Cuidamos mucho el detalle y el trato con el cliente.
+                    </p>
+            </div>
+
+            <div id="columna3"> 
+               <h1><center>SERVICIOS</center></h1>
+                    <p>Ofrecemos cortes de cabello exclusivos para niños, adolescentes y caballeros.
+                        Te cortamos el pelo a máquina o a tijera, te arreglamos la barba o te afeitamos
+                        con un ritual de toallas que te sorprenderá.
+                    </p>
+            </div>
+
+            <div id="columna4">
+                <h1><center>RESERVAS</center></h1>
+                    <p>Ya puedes realizar tu reserva mediante internet, además de por teléfono.
+                    Entra con tus datos personales y reserva ahora en cualquiera de nuestras barberías.
+                    </p>
+            </div>
+        </div> 
+
+        <h1 style=" text-align: center;"> Disfrute de Nuestros Videos</h1>
+        
+        <div class="video-responsive">
+            <iframe src="//www.youtube.com/embed/srAp9pzvRoE" frameborder="0" allowfullscreen width="560" height="315"></iframe>
+        </div>
+        
+        <nav> 
+                <ul>
+                    <li> <a href=""> Inicio</a></li>
+                    <li> <a href=""> Nosotros</a></li>
+                    <li> <a href=""> Galeria</a></li>
+                    <li> <a href=""> Contactenos</a></li>
+                    <li> <a href=""> Login</a></li>
+                    <li> <a href="" id="Registro"> Registro</a></li>
+                </ul>
+            </nav>
+        
+        
+        <footer>
+            <p>Copyright &copy; LatinosBarberShop 2016</p>
+        </footer>
+
+    </body>
+</html>
